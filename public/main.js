3a034e2a5f96c7694a88853615ce5101daa10414
이지윤 <JIYUN@Jiyunui-iMac.local>
2017-10-09 06:23:46 +0900

    first commit

---
+++ public/main.js
@@ -0,0 +1,493 @@
+const useTrickleICE = false;
+
+let localVideo = document.getElementById('local_video');
+let remoteContainer = document.getElementById('remote_container');
+let stateSpan = document.getElementById('state_span');
+let localStream = null;
+let peerConnection = null;
+
+// *** 기본설정 ***
+// 웹브라우저별로 WebRTC getUserMedia API 호출
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+
+RTCSessionDescription = window.RTCSessionDescription || window.webkitRTCSessionDescription || window.mozRTCSessionDescription;
+
+// Chrome 브라우저의 경우, PlanB를 사용해야 하므로 체크박스 표시
+if (window.window.webkitRTCPeerConnection) {
+    document.getElementById('plan_b_check').checked = true;
+}
+
+// *** 웹소켓 ***
+let ws = new WebSocket('ws://localhost:3001/');
+
+ws.onopen = function (event) {
+    console.log('ws open()');
+};
+
+ws.onerror = function (error) {
+    console.error('ws onerror(), ERROR: ' + error);
+};
+
+// 상대방이 들어오는 것을 감지하기 위한 코드
+ws.onmessage = function (p1) {
+    console.log('ws onmessage() DATA: ' + p1.data);
+
+    let message = JSON.parse(p1.data);
+
+    if (message.type === 'response') {
+        console.warn('NOT USED');
+    }
+
+    if (message.type === 'offer') {
+        console.log('오퍼 받음...');
+
+        let offer = new RTCSessionDescription(message);
+        setOffer(offer);
+    } else if (message.type === 'answer') {
+        console.log('응답 받음...');
+        console.warn('NOT USED');
+    }
+};
+
+function getUsePlanB() {
+    let checkbox = document.getElementById('plan_b_check');
+    return (checkbox.checked === true);
+}
+
+
+// *** 미디어 핸들링 ***
+// 로컬 비디오 스타트
+function startVideo() {
+    getDeviceStream({
+        audio: true,
+        video: true
+    })
+        .then( function (stream) {
+            // Chrome 브라우저에서는 stream을 바로 video src로 사용할 수 없다
+            localStream = stream;
+            logStream('localstream', stream);
+            playVideo(localVideo, stream);
+
+            updateButtons();
+        })
+        .catch( function (error) {
+            console.error('getUserMedia ERROR: ' + error);
+            return;
+        });
+}
+
+// 로컬 비디오 정지
+function stopVideo() {
+    pauseVideo(localVideo);
+    stopLocalStream(localStream);
+    localStream = null;
+
+    updateButtons();
+}
+
+// 로컬 스트림 정지
+function stopLocalStream(stream) {
+   let tracks = stream.getTracks();
+
+   if (! tracks) {
+       console.warn('NO tracks');
+       return;
+   }
+
+   for (let t of tracks){
+       t.stop();
+   }
+}
+
+// 브라우저로부터 스트림 따옴
+function getDeviceStream(option) {
+    if('getUserMedia' in navigator.mediaDevices) {
+        console.log('navigator.mediaDevices.getUserMedia');
+        return navigator.mediaDevices.getUserMedia(option);
+    } else {
+        console.log('navigator.getUserMedia를 Promise로 감쌈');
+        return new Promise(function (resolve, reject) {
+            navigator.getUserMedia(
+                option,
+                resolve,
+                reject
+            );
+        });
+    }
+}
+
+// 비디오 재생
+function playVideo(element, stream) {
+    if('srcObject' in element) {
+        element.srcObject = stream;
+    } else {
+        element.src = window.URL.createObjectURL(stream);
+    }
+
+    element.play();
+    element.volume = 0;
+}
+
+// 비디오 멈춤
+function pauseVideo(element) {
+    element.pause();
+
+    if('srcObject' in element) {
+        element.srcObject = null;
+    } else {
+        if (element.src && (element.src !== '')) {
+            window.URL.revokeObjectURL(element.src);
+        }
+
+        element.src = '';
+    }
+}
+
+
+// *** 시그널링 ***
+function sendSdp(sessionDescription) {
+    console.log('*** SDP 보냄 ***');
+
+    const jsonSDP = sessionDescription.toJSON();
+    jsonSDP.planb = getUsePlanB();
+    console.log('보내는 SDP: ' + jsonSDP);
+
+    sendJson(jsonSDP);
+}
+
+function sendJson(json) {
+    // 웹소켓을 통해 전송
+    const str = JSON.stringify(json);
+    ws.send(str);
+}
+
+// *** 커넥션 핸들링 ***
+function prepareNewConnection() {
+    // ICE Server는 Google의 Public STUN Server를 사용할 수도 있음
+    // 예시) { 'iceServers': [{'url': 'stun:stun.l.google.com:19302'}] };
+    let pc_config = { 'iceServers': [] };
+    let peer = new RTCPeerConnection(pc_config);
+
+    // 리모트 스트림 받아옴
+    if ('ontrack' in peer) {
+        peer.ontrack = function (event) {
+            console.log('*** peer.ontrack() ***');
+
+            let stream = event.streams[0];
+            logStream('remotestream of ontrack()', stream);
+
+            if ((stream.getVideoTracks().length > 0) && (stream.getAudioTracks().length > 0)) {
+                addRemoteVideo(stream.id, stream);
+            }
+        };
+    } else {
+        peer.onaddstream = function (event) {
+            console.log('*** peer.onaddstream() ***');
+
+            let stream = event.stream;
+            logStream('remotestream of onaddstream()', stream);
+
+            addRemoteVideo(stream.id, stream);
+        };
+    }
+
+    // 로컬 ICE 후보자 가져옴
+    peer.onicecandidate = function (event) {
+        // Trickle ICE: 모든 후보자들이 모이기까지 기다리지 않고, 한명+a의 후보자가 모이면 전체 프로세스를 병렬처리한다
+        // Vanilla ICE: 구글링실패ㅠ 추가바람
+
+        if (event.candidate) {
+            console.log(event.candidate);
+            // Trickle ICE의 경우 ICE candidate를 상대쪽으로 보낸다
+            // 그러나 아직 코드가 없어서 아무것도 안함
+
+            // Vanilla ICE의 경우 아무것도 하지 않는다
+        } else {
+            console.log('ICE 이벤트 없음');
+
+            if (useTrickleICE) {
+                // Trickle ICE의 경우 아무것도 하지 않는다
+            } else {
+                // Vanilla ICE의 경우 ICE candidate를 포함한 SDP를 상대쪽으로 보낸다
+                sendSdp(peer.localDescription);
+            }
+        }
+    };
+
+    // SDP를 교환해야 할 때
+    peer.onnegotiationneeded = function (event) {
+        console.log('*** onnegotiationneeded() ***');
+        console.warn('*** 무시 ***');
+    };
+
+    // 다른 이벤트들
+    peer.onicecandidateerror = function (event) {
+        console.error('ICE candidate ERROR: ' + event);
+    };
+    
+    peer.onsignalingstatechange = function () {
+        console.log('시그널링 상태: ' + peer.signalingState);
+    };
+    
+    peer.oniceconnectionstatechange = function () {
+        console.log('ICE 커넥션 상태: ' + peer.iceConnectionState);
+        showState('ICE 커넥션 상태: ' + peer.iceConnectionState);
+
+        if (peer.iceConnectionState === 'disconnected') {
+            console.log('연결 끊김, 재연결 시도 대기');
+        } else if (peer.iceConnectionState === 'failed') {
+            console.log('연결 실패, 연결 시도 포기');
+            dissconnect();
+        }
+    };
+
+    peer.onicegatheringstatechange = function () {
+        console.log('ICE gathering 상태: ' + peer.iceGatheringState);
+    };
+
+    peer.onconnectionstatechange = function () {
+        console.log('커넥션 상태: ' + peer.connectionState);
+    };
+
+    peer.onremovestream = function (event) {
+        console.log('*** peer.onremovestream() ***');
+
+        let stream = event.stream;
+        removeRemoteVideo(stream.id, stream);
+    };
+
+    // 로컬 스트림 추가
+    if (localStream) {
+        console.log('로컬 스트림 추가...');
+
+        if('addTrack' in peer) {
+            console.log('addTrack() 사용');
+
+            let tracks = localStream.getTracks();
+            for (let t of tracks) {
+                let sender = peer.addTrack(t, localStream);
+            }
+        } else {
+            console.log('addStrea() 사용');
+            peer.addStream(localStream);
+        }
+    } else {
+        console.warn('로컬 스트림 없음, 그러나 계속 진행');
+    }
+
+    return peer;
+}
+
+function setOffer(sessionDescription) {
+    let waitForCandidates = true;
+
+    if (peerConnection) {
+        console.log('peerConnection 이미 존재, 재사용합니다');
+
+        if (peerConnection.remoteDescription && (peerConnection.remoteDescription.type === 'offer')) {
+            // Chrome 브라우저를 사용하는 경우 Vanilla ICE를 사용하지만 재오퍼 받았으므로 후보자들을 기다리지 않는다
+            if (getUsePlanB()) {
+                waitForCandidates = false;
+            }
+        }
+    } else {
+        console.log('새로운 PeerConnection 준비');
+
+        peerConnection = prepareNewConnection();
+    }
+
+    peerConnection.setRemoteDescription(sessionDescription)
+        .then( function () {
+            console.log('setRemoteDescription(offer) 성공');
+            makeAnswer(waitForCandidates);
+        })
+        .catch( function (error) {
+            console.error('setRemoteDescription(offer) ERROR: ' + error);
+        });
+}
+
+function makeAnswer(waitForCandidates) {
+    console.log('응답 보냄, remote session description 생성하는 중...');
+
+    if (! peerConnection) {
+        console.error('peerConnection 존재하지 않음!');
+        return;
+    }
+
+    peerConnection.createAnswer()
+        .then( function (sessionDescription) {
+            console.log('createAnswer() 성공');
+            return peerConnection.setLocalDescription(sessionDescription);
+        })
+        .then( function () {
+            console.log('setLocalDescription() 성공');
+
+            if (useTrickleICE) {
+                // Trickle ICE의 경우 초기 이니셜 SDP를 상대쪽으로 보낸다
+                // 그러나 아직 코드가 없어서 아무것도 안함
+            } else {
+                // Vanilla ICE의 경우 아직 SDP를 보내지 않고 대기한다
+
+                // 만약 재오퍼를 받았다면, ICE 후보자가 더이상 오지 않을 것이므로 이때 SDP를 보낸다
+                if (! waitForCandidates) {
+                    sendSdp(peerConnection.localDescription);
+                }
+            }
+        })
+        .catch( function (error) {
+            console.error(error);
+        });
+}
+
+// PeerConnection 스타트
+function connect() {
+    callWithCapabilitySDP();
+    updateButtons();
+}
+
+function callWithCapabilitySDP() {
+    peerConnection = prepareNewConnection();
+    peerConnection.createOffer()
+        .then( function (sessionDescription) {
+            console.log('createOffer() 성공');
+
+            // setLocalDescription() 없이, 서버로 보낸다
+            console.log('Capability SDP 부르는 중...');
+            sendJson({
+                type: 'call',
+                planb: getUsePlanB(),
+                capability: sessionDescription.sdp
+            });
+        })
+        .catch( function (error) {
+            console.error('callWithCapabilitySDP() ERROR: ' + error);
+        });
+}
+
+// PeerConnection 닫음
+function dissconnect() {
+    sendJson({
+        type: 'bye'
+    });
+
+    if (peerConnection) {
+        console.log('끊습니다');
+
+        peerConnection.close();
+        peerConnection = null;
+
+        removeAllRemoteVideo();
+    } else {
+        console.warn('peer가 존재하지 않음');
+    }
+
+    updateButtons();
+}
+
+function showState(state) {
+    stateSpan.innerText = state;
+}
+
+function logStream(message, stream) {
+    console.log(message + ': ID = ' + stream.id);
+
+    let videoTracks = stream.getVideoTracks();
+
+    if (videoTracks) {
+        console.log('videoTracks.length = ' + videoTracks.length);
+
+        videoTracks.forEach( function (t) {
+            console.log(' track.id = ' + t.id);
+        });
+    }
+
+    let audioTracks = stream.getAudioTracks();
+
+    if (audioTracks) {
+        console.log('audioTracks.length = ' + audioTracks.length);
+
+        audioTracks.forEach( function (t) {
+            console.log(' track.id = ' + t.id);
+        });
+    }
+}
+
+// 리모트 비디오 화면에 추가
+function addRemoteVideo(id, stream) {
+    let element = document.createElement('video');
+
+    remoteContainer.appendChild(element);
+    element.id = 'remote_' + id;
+    element.width = 320;
+    element.height = 240;
+    element.srcObject = stream;
+    element.play();
+    element.volume = 0;
+    element.controls = true;
+}
+
+function removeRemoteVideo(id, stream) {
+    console.log('*** removeRemoteVideo() ID = ' + id);
+
+    let element = document.getElementById('remote_' + id);
+
+    if (element) {
+        element.pause();
+        element.srcObject = null;
+        remoteContainer.removeChild(element);
+    } else {
+        console.log('child element 찾을 수 없음');
+    }
+}
+
+function removeAllRemoteVideo() {
+    while (remoteContainer.firstChild) {
+        remoteContainer.firstChild.pause();
+        remoteContainer.firstChild.srcObject = null;
+        remoteContainer.removeChild(remoteContainer.firstChild);
+    }
+}
+
+function updateButtons() {
+    if (peerConnection) {
+        disableElement('start_video_button');
+        disableElement('stop_video_button');
+        disableElement('connect_button');
+        enableElement('disconnect_button');
+        disableElement('plan_b_check');
+    } else {
+        if (localStream) {
+            disableElement('start_video_button');
+            enableElement('stop_video_button');
+            enableElement('connect_button');
+        } else {
+            enableElement('start_video_button');
+            disableElement('stop_video_button');
+            disableElement('connect_button');
+        }
+
+        disableElement('disconnect_button');
+        enableElement('plan_b_check');
+    }
+}
+
+function enableElement(id) {
+    let element = document.getElementById(id);
+
+    if (element) {
+        element.removeAttribute('disabled');
+    }
+}
+
+function disableElement(id) {
+    let element = document.getElementById(id);
+
+    if (element) {
+        element.setAttribute('disabled', '1');
+    }
+}
+
+updateButtons();
+console.log('*** 준비완료! ***');
---
+++ server.js
@@ -0,0 +1,271 @@
+'use strict';
+
+const WebSocketServer = require('ws').Server;
+const wsServer = new WebSocketServer({ port: 3001 });
+console.log('웹소켓 서버 스타트! 3001번 포트입니당');
+
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+const webServer = app.listen(3000, function () {
+    console.log('웹서버 스타트! http://localhost:' + webServer.address().port + '/');
+});
+
+const mediasoup = require('mediasoup');
+const RTCPeerConnection = mediasoup.webrtc.RTCPeerConnection;
+const RTCSessionDescription = mediasoup.webrtc.RTCSessionDescription;
+const roomOptions = require('./data/options').roomOptions;
+const peerCapabilities = require('./data/options').peerCapabilities;
+
+let selfId = null;
+let soupRoom = null;
+let Connections = new Array();
+let clientIndex = 0;
+
+// 미디어숲
+let server = mediasoup.Server();
+server.createRoom(roomOptions)
+    .then( (room) => {
+        soupRoom = room;
+        console.log('server.createRoom() 성공!');
+        console.log('여기는 ' + getRoomName() + ' Room입니다!');
+    })
+    .catch( (err) => console.error('server.createRoom() ERROR: ' + err));
+
+// 웹소켓 서버
+function getId(ws) {
+    if (ws.additionalId) {
+        return ws.additionalId;
+    } else {
+        clientIndex++;
+        ws.additionalId = 'member_' + clientIndex;
+        return ws.additionalId;
+    }
+}
+
+function getClientCount() {
+    return wsServer.clients.length;
+}
+
+wsServer.on('connection', function connection(ws) {
+    console.log('클라이언트 연결됨. ID = ' + getId(ws) + ', 총 클라이언트는 ' + getClientCount());
+
+    ws.on('close', function () {
+        console.log('클라이언트 연결 닫힘. ID = ' + getId(ws) + ', 총 클라이언트는 ' + getClientCount());
+        cleanUpPeer(ws);
+    });
+
+    ws.on('error', function (err) {
+        console.error('에러! ' + err);
+    });
+
+    ws.on('message', function incoming(data) {
+        const inMessage = JSON.parse(data);
+        const id = getId(ws);
+        console.log('ID %s 받음, 타입은 %s', id, inMessage.type);
+
+        if (inMessage.type === 'call') {
+            console.log('ID %s 로부터 콜 받음', id);
+            let message = { sendto: id, type: 'response' };
+            console.log('ID %s 에게 오퍼 보냄', id);
+
+            // PeerConnection 준비, SDP 보냄
+            // SDP(Session Description Protocol): 연결하고자 하는 Peer 서로간의 미디어와 네트워크에 관한 정보를 이해하기 위해 사용된다
+            // offer와 answer시에 주고받는 방식
+            // 참고로 offer answer는 JSEP(Javascript Session Establishment Protocol)에 따른다
+            // 자세한 설명은 링크(https://cryingnavi.github.io/WebRTC-SDP/)
+            const downOnlyRequested = false;
+            preparePeer(ws, inMessage, downOnlyRequested);
+        } else if (inMessage.type === 'call_downstream') {
+            // 리얼타임 스트리밍을 위해 요청된 다운스트림만 허용
+            const downOnlyRequested = true;
+            preparePeer(ws, inMessage, downOnlyRequested);
+        } else if (inMessage.type === 'offer') {
+            console.log('ID %s 로부터 오퍼 받음', id);
+            console.error('절대 오퍼 받아서는 안됨');
+        } else if (inMessage.type === 'answer') {
+            console.log('ID %s 로부터 응답 받음', id);
+            handleAnswer(ws, inMessage);
+        } else if (inMessage.type === 'candidate') {
+            console.error('절대 후보 받아서는 안됨');
+        } else if (inMessage.type === 'bye') {
+            cleanUpPeer(ws);
+        }
+    });
+
+    sendback(ws, { type: 'welcome' });
+});
+
+// 웹소켓을 통해 보냄
+function sendback(ws, message) {
+    let str = JSON.stringify(message);
+    ws.send(str);
+}
+
+function preparePeer(ws, message, downOnly) {
+    const id = getId(ws);
+    const planb = message.planb;
+    const capabilitySDP = message.capability;
+
+    let peer = soupRoom.Peer(id);
+    let peerconnection = new RTCPeerConnection({
+        peer: peer,
+        usePlanB: planb
+    });
+    console.log('*** RTCPeerConnection 생성 ***');
+    console.log('*** 방 안에 peer들 = ' + soupRoom.peers.length);
+
+    peerconnection.on('close', function (err) {
+        console.log('*** PeerConnection.closed, err: ' + err);
+    });
+
+    peerconnection.on('signalingstatechange', function () {
+        console.log('*** PeerConnection.signalingstatechanged, state: ' + peerconnection.signalingState);
+    });
+
+    peerconnection.on('negotiationneeded', () => {
+        console.log('*** PeerConnection.negotiationneeded!!! id: ' + id);
+
+        // 여기서 SDP 보냄
+        sendOffer(ws, peerconnection, downOnly);
+    });
+
+    peerconnection.setCapabilities(capabilitySDP)
+        .then( () => {
+            console.log('peerconnection.setCapabilities() OK');
+            addPeerConnection(id, peerconnection);
+            sendOffer(ws, peerconnection);
+        })
+        .catch( (err) => {
+            console.log('peerconnection.setCapabilities() ERROR: ' + err);
+            peerconnection.close();
+        })
+}
+
+function sendOffer(ws, peerconnection, downOnly) {
+    const id = getId(ws);
+    console.log('ID %s 에 오퍼', id);
+
+    let offerOption = {
+        offerToReceiveAudio: 1,
+        offerToReceiveVideo: 1
+    };
+
+    if (downOnly) {
+        offerOption.offerToReceiveAudio = 0;
+        offerOption.offerToReceiveVideo = 0;
+    }
+
+    peerconnection.createOffer(offerOption)
+        // PeerConnection으로부터 SD 생성
+        .then( (desc) => {
+            return peerconnection.setLocalDescription(desc);
+        })
+
+        // 생성된 SD를 상대방에게 전송하고, 전송한 SD를 PeerConnection 객체의 local로 설정
+        .then( () => {
+            dumpPeer(peerconnection.peer, 'peer.dump after createOffer');
+            sendSDP(ws, peerconnection.localDescription);
+        })
+        .catch( (error) => {
+            console.error('참여자에게 보내는 SDP 오퍼 핸들링 에러: %s', error);
+
+            // peerconnection 닫음
+            peerconnection.reset();
+            peerconnection.close();
+            deletePeerConnection(id);
+        });
+}
+
+function handleAnswer(ws, message) {
+    const id = getId(ws);
+
+    let peerconnection = getPeerConnection(id);
+
+    if (! peerconnection) {
+        console.warn('경고: 커넥션 찾을 수 없음, ID = ' + id);
+        return;
+    }
+
+    let desc = new RTCSessionDescription({
+        type: 'answer',
+        sdp: message.sdp
+    });
+
+    // offer를 통해 건네받은 SD를 PeerConnection의 remote로 설정
+    peerconnection.setRemoteDescription(desc)
+        .then( function () {
+            console.log('setRemoteDescription for Answer OK, ID = ' + id);
+            console.log('*** 방 안에 peer들 = ' + soupRoom.peers.length);
+
+            // answer를 위해 생성한 SD로 상대방에게 answer를 전달
+            dumpPeer(peerconnection.peer, 'peer.dump after setRemoteDescription(answer):');
+        })
+        .catch( (err) => {
+            console.error('setRemoteDescription for Answer ERROR: ' + err);
+        });
+}
+
+function dumpPeer(peer, caption) {
+    console.log(caption + '! 전송 = %d, 받는사람들 = %d, 보내는사람들 = %d', peer.transports.length, peer.rtpReceivers.length, peer.rtpSenders.length);
+
+    // 디버깅용
+    // peer.dump()
+    //     .then( (obj) => {
+    //         console.log(caption, obj)
+    //     });
+}
+
+function addPeerConnection(id, pc) {
+    Connections[id] = pc;
+}
+
+function getPeerConnection(id) {
+    const pc = Connections[id];
+    return pc;
+}
+
+function deletePeerConnection(id) {
+    delete Connections[id];
+}
+
+function cleanUpPeer(ws) {
+    const id = getId(ws);
+
+    let peerconnection = getPeerConnection(id);
+
+    if(! peerconnection) {
+        console.warn('경고: cleanUpPeer(id), 커넥션 찾을 수 없음, ID = ' + id);
+        return;
+    }
+
+    console.log('PeerConnection 닫힘, ID = ' + id);
+    peerconnection.close();
+    deletePeerConnection(id);
+
+    console.log('*** 방 안에 peer들 = ' + soupRoom.peers.length);
+}
+
+function sendSDP(ws, sessionDescription) {
+    const id = getId(ws);
+
+    let message = {
+        sendto: id,
+        type: sessionDescription.type,
+        sdp: sessionDescription.sdp
+    };
+
+    console.log('*** SDP 보냄 ***');
+    console.log('sendto: ' + message.sendto + ', type: ' + message.type);
+
+    sendback(ws, message);
+}
+
+// 룸 이름 리턴
+function getRoomName() {
+    var room = 'jiyun';
+    if (process.argv.length > 2) {
+        room = process.argv[2];
+    }
+    return room;
+}
